b230d582ddc9a0637fbd93c7cbeef0c02dac0f5a
Cameron <cbadwork@gmail.com>
2025-01-20 10:18:58 +0000

    moved registers to their own header

---
+++ registers.h
@@ -0,0 +1,20 @@
+#ifndf REGISTER_H
+#define REGISTER_H
+
+#include <stdint.h>
+
+enum {
+  R_R0 = 0,
+  R_R1,
+  R_R2,
+  R_R3,
+  R_R4,
+  R_R5,
+  R_R6,
+  R_R7,
+  R_PC,
+  R_COUND,
+  R_COUNT
+};
+
+#endif
