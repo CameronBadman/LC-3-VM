99d05121d2622dd91e2b1cf2b2829c307b22b760
Cameron <cbadwork@gmail.com>
2025-01-24 10:52:03 +0000

    remvoed old files

--- lc3.c
+++
@@ -1,3 +0,0 @@
-#include "lc3.h"
-
-uint16_t memory[MEMORY_MAX]; /* 65536 locations in memory  */
--- lc3.h
+++
@@ -1,9 +0,0 @@
-#ifndev LC #_H
-#define LC #_H
-
-#include "condition_flags.h"
-#include "opcodes.h"
-#include "registers.h"
-#include <stdint.h>
-
-#defin MEMORY_MAX(1 << 16)
